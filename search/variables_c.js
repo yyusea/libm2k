b1cb1cc5084e4ddeb3406db81a94f17839f033c5
Daniel Guramulta <daniel.guramulta@analog.com>
2019-07-23 09:18:06 +0300

    Update documentation to 90c5b6a

---
+++ search/variables_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['unit',['unit',['../structlibm2k_1_1analog_1_1_d_m_m___r_e_a_d_i_n_g.html#a9a84f2f1fe9497a3f21b94e96704b1f4',1,'libm2k::analog::DMM_READING']]],
+  ['uri',['uri',['../structlibm2k_1_1_c_o_n_t_e_x_t___i_n_f_o.html#a935ba950dcccf9ce28dceca872163aad',1,'libm2k::CONTEXT_INFO']]]
+];
